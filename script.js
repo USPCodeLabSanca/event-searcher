fad9b503f7411f7d974944516efab710ea1707a1
Rebeca Carvalho <rebeca.vcarvalho78@gmail.com>
2022-09-12 13:33:33 -0300

    Inicio do projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+function geoip(json){
+    let userip      = document.getElementById("user_ip");
+    let city = document.getElementById("user_city");
+    userip.textContent      = json.ip;
+    city.textContent = json.city;
+}
