3329a24b33d620713bf3c587a2507c8c6457b5e4
Thierry Araujo <thierry_ara@usp.br>
2022-09-14 18:12:13 -0300

    Trabalhando com TicketMaster

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import geohash2
+import requests
+import os
 
-app = Flask(__name__)
+app  = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/event', methods=['GET', 'POST'])
+def event():
+    if request.method == 'POST':
+        data = request.get_json()
+        
+        geoPoint = geohash2.encode(float(data['latitude']), float(data['longitude']), precision=9)
+        API_KEY = os.environ['API_KEY']
+        
+        radius = 100
+
+        events = requests.get(f'https://app.ticketmaster.com/discovery/v2/events.json?geoPoint={geoPoint}&radius={radius}&unit=km&apikey={API_KEY}')
+        print(f'https://app.ticketmaster.com/discovery/v2/events.json?geoPoint={geoPoint}&radius={radius}&unit=km&apikey={API_KEY}')
+        
+        events = events.json()
+        if '_embedded' in events:
+            events = events['_embedded']['events']
+            eventsNames = [{'name': event['name'], 'url': event['url']} for event in events]
+        else:
+            return {'description': 'ERROR'}, 500
+        print(eventsNames)
+        
+        return eventsNames, 200
+    return {'oi':'123'}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- script.js
+++
@@ -1,7 +0,0 @@
-function geoip(json){
-    let userip      = document.getElementById("user_ip");
-    let city = document.getElementById("user_city");
-    userip.textContent      = json.ip;
-    city.textContent = json.city;
-    alert("OI");
-}
