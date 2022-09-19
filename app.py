8cdc734cb08a58e9bc7b1b50396ca2042b0bb7f5
Rebeca Carvalho <rebeca.vcarvalho78@gmail.com>
2022-09-19 15:07:36 -0300

    Adição dos cards

--- app.py
+++ app.py
@@ -16,22 +16,27 @@ def event():
         
         geoPoint = geohash2.encode(float(data['latitude']), float(data['longitude']), precision=9)
         API_KEY = os.environ['API_KEY']
-        
-        radius = 100
+        radius = 5000
+
+        print(f'{API_KEY}')
 
         events = requests.get(f'https://app.ticketmaster.com/discovery/v2/events.json?geoPoint={geoPoint}&radius={radius}&unit=km&apikey={API_KEY}')
         print(f'https://app.ticketmaster.com/discovery/v2/events.json?geoPoint={geoPoint}&radius={radius}&unit=km&apikey={API_KEY}')
         
         events = events.json()
         if '_embedded' in events:
+            # print(events)
             events = events['_embedded']['events']
-            eventsNames = [{'name': event['name'], 'url': event['url']} for event in events]
+            eventsNames = [{'name': event['name'], 'image': event['images'][0]['url'], 'distance': event['distance'], 'city': event['_embedded']['venues'][0]['city']['name']} for event in events]
         else:
             return {'description': 'ERROR'}, 500
         print(eventsNames)
         
         return eventsNames, 200
-    return {'oi':'123'}
+
+@app.route('/events')
+def events():
+    return render_template('events.html',)
 
 
 if __name__ == "__main__":
