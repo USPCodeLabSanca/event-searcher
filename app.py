8c0eed588ecd4d39604f8f8135e536dc9304337c
Thierry Araujo <thierry_ara@usp.br>
2022-10-30 23:20:06 -0300

    link-paginas

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app  = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/eventData', methods=['GET', 'POST'])
+def eventData():
+    return render_template('eventData.html')
+
 @app.route('/event', methods=['GET', 'POST'])
 def event():
     if request.method == 'POST':
@@ -18,15 +22,36 @@ def event():
         API_KEY = os.environ['API_KEY']
         radius = 5000
 
-        print(f'{API_KEY}')
-
         events = requests.get(f'https://app.ticketmaster.com/discovery/v2/events.json?geoPoint={geoPoint}&radius={radius}&unit=km&apikey={API_KEY}')
     
         print(f'https://app.ticketmaster.com/discovery/v2/events.json?geoPoint={geoPoint}&radius={radius}&unit=km&apikey={API_KEY}')
         events = events.json()
+        eventsNames = []
+
         if '_embedded' in events:
             events = events['_embedded']['events']
-            eventsNames = [{'name': event['name'], 'image': event['images'][0]['url'], 'distance': event['distance'], 'city': event['_embedded']['venues'][0]['city']['name'], 'date': event['dates']['start']['localDate'], 'hour': None} if 'localTime' not in event['dates']['start'] else {'name': event['name'], 'image': event['images'][0]['url'], 'distance': event['distance'], 'city': event['_embedded']['venues'][0]['city']['name'], 'date': event['dates']['start']['localDate'], 'hour': event['dates']['start']['localTime']} for event in events]
+            for event in events:
+                data = {}
+                try:   data['name'] = event['name']
+                except: data['name'] = None
+                
+                try:    data['image'] = event['images'][0]['url']
+                except: data['image'] = None
+                
+                try:    data['distance'] = event['distance']
+                except: data['distance'] = None
+                
+                try:    data['city'] = event['_embedded']['venues'][0]['city']['name']
+                except: data['city'] = None
+                
+                try:    data['date'] = event['dates']['start']['localDate']
+                except: data['date'] = None
+                
+                try:    data['hour'] = event['dates']['start']['localTime']
+                except: data['hour'] = 0
+                
+                eventsNames.append(data)
+            #eventsNames = [{'name': event['name'], 'image': event['images'][0]['url'], 'distance': event['distance'], 'city': event['_embedded']['venues'][0]['city']['name'], 'date': event['dates']['start']['localDate'], 'hour': None} if 'localTime' not in event['dates']['start'] else {'name': event['name'], 'image': event['images'][0]['url'], 'distance': event['distance'], 'city': event['_embedded']['venues'][0]['city']['name'], 'date': event['dates']['start']['localDate'], 'hour': event['dates']['start']['localTime']} for event in events]  
         else:
             return {'description': 'ERROR'}, 500
         
